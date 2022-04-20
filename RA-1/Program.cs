b6d3062b597c14f7b9541ec04a8ca84b3be7a362
tGuevarra <104074409+tGuevarra@users.noreply.github.com>
2022-04-20 21:18:20 +0700

    Initial commit

---
+++ RA-1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RA
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please, enter your name: ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Please, enter your age: ");
+            var age = Console.ReadLine();
+
+            Console.WriteLine($"Hello World! This is {name}, and he is {age} y.o.");
+            Console.ReadLine();
+        }
+    }
+}
